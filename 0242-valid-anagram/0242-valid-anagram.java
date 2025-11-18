d8a5e6ad1207706c56d5c924b5adf301abb398fb
Saurabh Singh <saurabhsingh4510@gmail.com>
2025-11-18 13:31:53 +0530

    Time: 3 ms (90.75%), Space: 46.6 MB (5.69%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] arrs = s.toCharArray();
+        Arrays.sort(arrs);
+        String sorteds = new String(arrs); 
+
+        char[] arrt = t.toCharArray();
+        Arrays.sort(arrt);
+        String sortedt = new String(arrt);
+
+        if(sorteds.equals(sortedt)) return true;
+        return false;
+    }
+}
