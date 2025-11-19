2ff42976620b7efbafca3f8a30c556e79c64e520
Saurabh Singh <saurabhsingh4510@gmail.com>
2025-11-19 21:54:28 +0530

    Time: 0 ms (100%), Space: 48 MB (10.26%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -11,3 +11,25 @@ class Solution {
         return Math.pow(x, exp);
     }
 }
+
+/*
+class Solution {
+    public double myPow(double x, int n) {
+        long exp = n;
+        if (exp < 0) {
+            x = 1 / x;
+            exp = -exp;
+        }
+
+        double result = 1;
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                result *= x;   // multiply when bit is 1
+            }
+            x *= x;     // square the base
+            exp >>= 1;  // shift bits
+        }
+        return result;
+    }
+}
+*/
