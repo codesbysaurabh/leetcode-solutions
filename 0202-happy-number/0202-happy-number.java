bf928a634f31a5fdd11d936b04ada24f1eae7f37
Saurabh Singh <saurabhsingh4510@gmail.com>
2025-11-21 15:23:30 +0530

    Time: 0 ms (100%), Space: 42.4 MB (19.52%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -3,6 +3,10 @@ class Solution {
         int result=0;
         if (n == 1) return true;
         if (n == 4) return false;
+
+        // if (n == 1|| n == 7) return true;
+        // if (n < 10) return false;
+
         while(n!=0){
             result+=(n%10)*(n%10);
             n/=10;
