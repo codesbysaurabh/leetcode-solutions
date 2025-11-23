0e082a9c54ba24b3e5edd536cef8b34203421bbb
Saurabh Singh <saurabhsingh4510@gmail.com>
2025-11-23 18:22:12 +0530

    Time: 1 ms (99.95%), Space: 45.7 MB (42.51%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
@@ -1,4 +1,16 @@
+//i like how this one worjs and wished to save
 class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        int n = nums.length;
+        int result[] = new int[2], count[] = new int[n];
+        int a=0;
+        for (int i=0;i<nums.length;i++) 
+            if(++count[nums[i]]== 2) result[a++] = nums[i];
+        return result;
+    }
+}
+//My code, it had 6ms
+/* class Solution {
     public int[] getSneakyNumbers(int[] nums) {
         Arrays.sort(nums);
         int []s=new int[2];
@@ -9,4 +21,4 @@ class Solution {
         } 
         return s;
     }
-}
+} */
