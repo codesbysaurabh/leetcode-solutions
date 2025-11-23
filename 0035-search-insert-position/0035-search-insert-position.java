ae0cb3bb86221e06e3b39fbc79a95733883924c0
Saurabh Singh <saurabhsingh4510@gmail.com>
2025-11-23 14:35:17 +0530

    Time: 0 ms (100%), Space: 44.6 MB (19.03%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,3 +1,4 @@
+//0(N) just simply did it
 class Solution {
     public int searchInsert(int[] nums, int target) {
     int i=0;
@@ -7,3 +8,18 @@ class Solution {
         return i;
     }
 }
+
+//can do binary search for 0(log n)
+/* class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < target) l = mid + 1;
+            else r = mid - 1;
+        }
+        return l;
+    }
+} */
