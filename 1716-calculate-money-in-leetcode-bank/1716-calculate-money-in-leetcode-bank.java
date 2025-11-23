10a1328bfb58563592d5d199fda5aaf5e39456cb
Saurabh Singh <saurabhsingh4510@gmail.com>
2025-11-23 19:44:19 +0530

    Time: 2 ms (17.03%), Space: 42.1 MB (5.34%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -1,5 +1,17 @@
 class Solution {
     public int totalMoney(int n) {
+        int week=0,sum=0,day=0;
+        for(int i=0;i<n;i++){
+            if(i%7==0) week++;
+            day=week+(i%7);
+            sum+=day;
+        }
+        return sum;
+    }
+}
+
+/* class Solution {
+    public int totalMoney(int n) {
         int m=1,sum=0,d=1;
         for(int i=1;i<=7 && n!=0;i++){
             if(i==1) d=m;
@@ -12,4 +24,4 @@ class Solution {
         }
         return sum;
     }
-}
+} */
