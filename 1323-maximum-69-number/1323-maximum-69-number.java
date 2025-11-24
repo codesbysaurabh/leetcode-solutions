489f68e097e6e947da1409db41819b6097a4385a
Saurabh Singh <saurabhsingh4510@gmail.com>
2025-11-24 19:04:19 +0530

    Time: 1 ms (44.16%), Space: 42 MB (22.17%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -17,5 +17,33 @@ class Solution {
         }
         return nums;
     }
- 
 }
+
+/* class Solution {
+    public int maximum69Number (int num) {
+        int temp = num;
+        int index = -1;
+        int pos = 0;
+
+        while (temp > 0) {
+            if (temp % 10 == 6)
+                index = pos;
+            
+            temp /= 10;
+            pos++;
+        }
+
+        return (index == -1) ? num : num + 3 * (int)Math.pow(10, index);
+    }
+} */
+
+/* class Solution {
+    public int maximum69Number (int num) {
+        String snum = num + "";
+        int lastNine = snum.indexOf('6');
+        char[] chars = snum.toCharArray();
+        if (lastNine != -1) chars[lastNine] = '9';
+        return Integer.parseInt(new String(chars));
+
+    }
+} */
