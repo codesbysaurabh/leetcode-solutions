79f67e26934549844c1e385d01baa7819f9a061f
Saurabh Singh <saurabhsingh4510@gmail.com>
2025-11-28 01:30:46 +0530

    Time: 1 ms (100%), Space: 43.7 MB (25.19%) - LeetHub

--- 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.java
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.java
@@ -1,5 +1,25 @@
 class Solution {
     public int maxFreqSum(String s) {
+        int[] freq = new int[26];
+        for (char c : s.toCharArray())
+            freq[c - 'a']++;
+
+        int maxVowel = 0, maxConsonant = 0;
+        for (int i = 0; i < 26; i++) {
+            //char c = (char) ('a' + i);
+            if ("aeiou".indexOf(i + 'a') >= 0) 
+            //(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                maxVowel = Math.max(maxVowel, freq[i]);
+            else
+                maxConsonant = Math.max(maxConsonant, freq[i]);
+        }
+        return maxVowel + maxConsonant;
+    }
+}
+
+//3ms
+/* class Solution {
+    public int maxFreqSum(String s) {
         int a[]=new int[26];
         for(int i=0;i<s.length();i++) a[s.charAt(i)-'a']++;
         int vowels[]={a['a'-'a'],a['e'-'a'],a['i'-'a'],a['o'-'a'],a['u'-'a']};
@@ -9,4 +29,4 @@ class Solution {
         return a[a.length-1]+vowels[vowels.length-1];
 
     }
-}
+} */
