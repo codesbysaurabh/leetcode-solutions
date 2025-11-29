6c9139da76af10da59a4ede04e78a8504da026f1
Saurabh Singh <saurabhsingh4510@gmail.com>
2025-11-29 21:59:17 +0530

    Time: 46 ms (28.85%), Space: 48.3 MB (7.07%) - LeetHub

---
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String makeFancyString(String s) {
+        StringBuilder sb=new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            int len = sb.length();
+            if (len >= 2 && sb.charAt(len - 1) == ch && sb.charAt(len - 2) == ch) continue;
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
