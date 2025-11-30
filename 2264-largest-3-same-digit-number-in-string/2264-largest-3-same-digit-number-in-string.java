349f55f240bd354141f319f1d8f6ec09de805e1d
Saurabh Singh <saurabhsingh4510@gmail.com>
2025-11-30 16:20:06 +0530

    Time: 5 ms (32.49%), Space: 49.6 MB (5.38%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.java
@@ -4,14 +4,9 @@ class Solution {
         int cnt = 1;
         char best = '0' - 1; // smaller than any digit
         for (int i = 0; i < c.length - 1; i++) {
-            if (c[i] == c[i + 1]) {
-                cnt++;
-            } else {
-                cnt = 1;
-            }
-            if (cnt == 3) {
-                best = (char)Math.max(best, c[i]);
-            }
+            if (c[i] == c[i + 1]) cnt++;
+            else cnt = 1;
+            if (cnt == 3) best = (char)Math.max(best, c[i]);
         }
         if (best < '0') return ""; // no lucky triple found
         return "" + best + best + best;
