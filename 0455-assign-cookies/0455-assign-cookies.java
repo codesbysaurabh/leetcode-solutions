81e5ec50979dec74bff736e68c95c7dc307e1cff
Saurabh Singh <saurabhsingh4510@gmail.com>
2025-12-02 23:50:17 +0530

    Time: 14 ms (49.34%), Space: 51.1 MB (45.52%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,17 +1,25 @@
+
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        int n=g.length;
-        int m=s.length;
         Arrays.sort(g);
         Arrays.sort(s);
-        int l=0;
-        int r=0;
-        while(l<m &&r<n){
-            if(g[r]<=s[l]){
-                r++;
-            }
-            l++;
+        int rslt=0;
+        int i=0,j=0;
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) i++;
+            j++;
         }
-        return r;
+        return i;
     }
 }
+
+/* class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int rslt=0;
+        int i=0,j=0;
+        for(i=0;i<Math.min(s.length,g.length);i++) if(s[i]>=g[i]) rslt++;
+        return rslt;
+    }
+} */
