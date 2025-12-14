bf1b48bb8ec234d5234309727bb015cd11c31211
Saurabh Singh <saurabhsingh4510@gmail.com>
2025-12-14 15:31:13 +0530

    Time: 0 ms (100%), Space: 42.8 MB (9.06%) - LeetHub

--- 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
@@ -2,6 +2,25 @@ class Solution {
     public String toHex(int num) {
         if (num == 0) return "0";
 
+        char[] hex = "0123456789abcdef".toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        // Process exactly 32 bits
+        while (num != 0) {
+            int digit = num & 15;     // get last 4 bits
+            sb.append(hex[digit]);
+            num >>>= 4;              // unsigned shift
+        }
+
+        return sb.reverse().toString();
+    }
+}
+
+
+/* class Solution {
+    public String toHex(int num) {
+        if (num == 0) return "0";
+
         String hex = "0123456789abcdef";
         String ans = "";
 
@@ -12,4 +31,4 @@ class Solution {
 
         return ans;
     }
-}
+} */
