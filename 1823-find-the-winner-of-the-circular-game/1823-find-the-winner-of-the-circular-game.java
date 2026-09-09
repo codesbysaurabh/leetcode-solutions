692aa9937e8b398018ff1a0840011a3efc242b5f
Saurabh Singh <saurabhsingh4510@gmail.com>
2026-09-09 15:59:37 +0530

    Time: 0 ms (100%), Space: 42.2 MB (77.76%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,12 +1,18 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=1;i<=n;i++) q.add(i);
+        
+        // Using QUEUE
+        // Queue<Integer> q = new LinkedList<>();
+        // for(int i=1;i<=n;i++) q.add(i);
 
-        while(q.size()>1){
-            for(int i=1;i<=k-1;i++) q.add(q.remove());
-            q.remove();
-        }
-        return q.peek();
+        // while(q.size()>1){
+        //     for(int i=1;i<=k-1;i++) q.add(q.remove());
+        //     q.remove();
+        // }
+        // return q.peek();
+
+        int ans = 0;
+        for(int i=2; i<=n; i++) ans = (ans+k)%i;
+        return ans+1;
     }
 }
